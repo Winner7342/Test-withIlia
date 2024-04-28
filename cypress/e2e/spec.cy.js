bd860be1dab29570c5380a105003dc97a375e195
Hallyyev Eziz <ezizhallyev@gmail.com>
2024-04-27 20:48:32 -0500

    Message to Iliya please don't forget pay your bill

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.request({
+
+//       url: "https://google.com",
+//       method: "GET",
+//       header: {
+
+//       }
+//     })
+//       .then(response => {
+//         expect(response.status).to.equal(200)
+//       })
+//   })
+// })
+
+
+function greet(firstName) {
+  console.log('I am growing step by step ' + ' ' + firstName)
+}
+
+greet('Ilia');
